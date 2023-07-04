d31f66ebc4f8e86b19b2727de4b91e78e63e3307
Gabriell@DESKTOP-2QII8SV.(none) <gabriel.ser7@gmail.com>
2023-07-04 16:25:32 +0300

    Last edit

---
+++ TyresWebApp/TyresWebApp/Interfaces/ITyreRepository.cs
@@ -0,0 +1,13 @@
+﻿using TyresWebApp.Models;
+
+namespace TyresWebApp.Interfaces
+{
+    public interface ITyreRepository
+    {
+  
+        bool TyreExists(int id);
+        bool CreateTyre(TyreModels tyre);
+
+        bool Save();
+    }
+}
